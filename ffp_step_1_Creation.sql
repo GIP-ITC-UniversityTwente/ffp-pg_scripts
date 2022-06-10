cf576208d741322d01313145d50f0f5d7190f97b
Javier Morales <j.morales@utwente.nl>
2022-06-10 11:12:28 +0200

    Update ffp_step_1_Creation.sql

--- ffp_step_1_Creation.sql
+++ ffp_step_1_Creation.sql
@@ -14,6 +14,7 @@ create extension if not exists postgis;
 create extension if not exists postgis_topology;
 create extension if not exists "uuid-ossp";
 create extension if not exists tablefunc;
+create extension if not exists unaccent;
 create schema if not exists survey;
 create schema if not exists load;
 create schema if not exists inspection;
